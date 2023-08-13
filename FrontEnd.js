a3d2fcf35c1f661608ed22b60c48e095c8036c48
SACHIN SHARMA <117117742+DEVNINJASACHIN@users.noreply.github.com>
2023-08-13 23:16:28 +0530

    Update FrontEnd.js

--- FrontEnd.js
+++ FrontEnd.js
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html lang="en-GB" class="no-js">
-<head>
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
