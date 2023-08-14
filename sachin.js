721875f82973712c7cbeb9f61c3eb9ead9ade556
SACHIN SHARMA <117117742+DEVNINJASACHIN@users.noreply.github.com>
2023-08-14 21:17:29 +0530

    Add files via upload

---
+++ sachin.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seat Booking</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="scripts.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Seat Booking</h1>
+        <div id="cinema-hall" class="d-flex flex-wrap"></div>
+        <button id="submit" class="btn btn-primary mt-3">Book Seats</button>
+    </div>
+</body>
+</html>
